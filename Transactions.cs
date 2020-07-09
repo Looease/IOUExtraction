175e4d04c163eff8221ac76d0b2c53b6a5f090da
Louise <louise.gilligan@zoopla.co.uk>
2020-07-09 17:07:48 +0100

    Added Class files and CSV file

---
+++ Transactions.cs
@@ -0,0 +1,15 @@
+using System.Transactions;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IOU_Extraction
+{
+    class FileReader
+    {
+        public static List <Transaction>()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
